9ea50aed9d196d2c026fcd4792c5231010364d40
ZeeshanGeoPk <zeeshangeopk786@gmail.com>
2026-05-22 19:32:37 +0500

    first commit

---
+++ app/src/main/java/com/example/streambert/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.streamzee
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.streamzee.ui.theme.streamzeeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            streamzeeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun greetingPreview() {
+    streamzeeTheme {
+        greeting("Android")
+    }
+}
